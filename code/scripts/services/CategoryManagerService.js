4e85122f33b30dc88d449003a590d31c462b2530
Teodor Lupu <luputeodor2@gmail.com>
2022-12-27 17:06:51 +0200

    Category Manager Enclave Operations

--- code/scripts/services/CategoryManagerService.js
+++ code/scripts/services/CategoryManagerService.js
@@ -1,75 +1,53 @@
 class CategoryManagerService {
 
-    constructor() {
-        const HostBootScript = require("boot-host").HostBootScript;
-        new HostBootScript("category-manager-service");
+    constructor(mainEnclave) {
+        this.enclave = mainEnclave;
     }
 
-    addCategory(categoryDetails, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "createCategoryDossier", categoryDetails).onReturn(callback);
-    }
-
-    importCategory(categorySeed, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "importCategory", categorySeed).onReturn(callback);
+    addCategory(name, callback) {
+        this.enclave.insertRecord("categories", name, {data: "encrypted"}, {name: name}, callback);
     }
 
     listCategories(callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "listCategories").onReturn((err, mountedCategories) => {
+        this.enclave.getAllRecords("categories", callback);
+    }
+
+    addPassword(password, callback) {
+        this.enclave.insertRecord("passwords", password.domain, {data: "encrypted"}, password, callback);
+    }
+
+    listCategoryPasswords(categoryName, callback) {
+        this.enclave.filterRecords("passwords", ["category == categoryName"], callback);
+    }
+
+    editPassword(domain, password, callback) {
+        this.enclave.deleteRecord("passwords", domain, (err) => {
             if (err) {
                 return callback(err);
             }
-
-            let categories = [];
-
-            let getCategoryDetails = (mountedCategory) => {
-                this.listCategoryPasswords("/categories/" + mountedCategory.path, (err, passwords) => {
-                    if (err) {
-                        return callback(err);
-                    }
-                    categories.push({
-                        ...mountedCategory,
-                        passwords: passwords,
-                        passwordsCount: passwords.length
-                    });
-
-                    if (mountedCategories.length > 0) {
-                        return getCategoryDetails(mountedCategories.shift());
-                    }
-                    callback(undefined, categories);
-                });
-            }
-
-            if (mountedCategories.length > 0) {
-                return getCategoryDetails(mountedCategories.shift());
-            }
-            callback(undefined, categories);
+            this.addPassword(password, callback);
         });
     }
 
-    addPassword(categoryPath, passwordDetails, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "createPassword", categoryPath, passwordDetails).onReturn(callback);
-    }
-
-    importPassword(categoryPath, passwordSeed, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "importPassword", categoryPath, passwordSeed).onReturn(callback);
-    }
-
-    listCategoryPasswords(mountedCategoryPath, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "listCategoryPasswords", mountedCategoryPath).onReturn(callback)
-    }
-
-    editPassword(categoryPath, passwordPath, passwordDetails, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "editPassword", categoryPath, passwordPath, passwordDetails).onReturn(callback)
-    }
-
-    deletePassword(categoryPath, passwordPath, callback) {
-        $$.interaction.startSwarmAs("test/agent/007", "categoriesSwarm", "deletePassword", categoryPath, passwordPath).onReturn(callback)
+    deletePassword(domain, callback) {
+        this.enclave.deleteRecord("passwords", domain, callback);
     }
 }
 
-let categoryManagerService = new CategoryManagerService();
-let getCategoryManagerServiceInstance = function () {
-    return categoryManagerService;
+let categoryManagerService;
+let getCategoryManagerServiceInstance = function (callback) {
+    if (!categoryManagerService) {
+        const opendsu = require("opendsu");
+        let securityContext = opendsu.loadAPI("sc");
+        return securityContext.getMainEnclave((err, mainEnclave) => {
+            if (err) {
+                return callback(err);
+            }
+            categoryManagerService = new CategoryManagerService(mainEnclave);
+            return callback(undefined, categoryManagerService);
+        });
+    }
+    callback(undefined, categoryManagerService);
 }
 
 export {
